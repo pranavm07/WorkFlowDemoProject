7c8c7848068c7b51df126e1e602d748e8bae2934
pranav <pranav@nalashaa.com>
2018-07-10 14:56:26 +0530

    first commit

---
+++ LogEntity/Helper/AuditLogEntityModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogEntity.Helper
+{
+    public class AuditLogEntityModel: DbContext
+    {
+        public AuditLogEntityModel(string connectionString) :base(connectionString)
+        {
+
+        }
+
+        public AuditLogEntityModel(DbConnection connection, bool contextOwnsConnection):base(connection, contextOwnsConnection)
+        {
+
+        }
+    }
+}
---
+++ LogEntity/Helper/ConnectinString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogEntity.Helper
+{
+    public class ConnectinString
+    {
+        public static string getConnectionString()
+        {
+            string constr = string.Empty;
+
+            constr = new AuditLogDbEntities().Database.Connection.ConnectionString;
+
+
+            return constr;
+        }
+    }
+}
